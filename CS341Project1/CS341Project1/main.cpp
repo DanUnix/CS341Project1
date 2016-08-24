fda39f8de02236ab56287f65992fa52edbfdf0ba
Daniel Pulley <Daniel Pulley>
2016-08-23 22:54:02 -0500

    Add CS341 Project 1 files
    Add the new files for the first CS341 project

---
+++ CS341Project1/CS341Project1/main.cpp
@@ -0,0 +1,36 @@
+//
+// Chicago Crime Analysis: top-5 crimes
+// 
+// <<Daniel Pulley>>
+// U. of Illinois, Chicago
+// CS341, Fall 2016
+// HW #01
+//
+
+// Preprocessor Directives
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// using namespace std;
+
+using std::string;
+using std::ifstream;
+using std::vector;
+using std::stringstream;
+using std::getline;
+using std::stoi;
+using std::sort;
+using std::cout;
+using std::endl;
+
+// Main Function
+int main()
+{
+
+}
+// End of Main Function
