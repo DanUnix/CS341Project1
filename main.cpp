3129e7a9d145af02e678dabcb689cc0aea4d7bc3
Daniel Pulley <Daniel Pulley>
2016-08-23 22:53:25 -0500

    Ignore uneeded Visual C++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+}
