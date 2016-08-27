38d7c378b11217066e53bc89060209c258bc6fa7
Daniel Pulley <dpulley@uic.edu>
2016-08-27 11:30:28 -0500

    Update main function
    create Vector of type Crime class object to hold CSV data
    
    update Arrest and DomesticViolence variables in Crime class from bool to string
    
    loop through CSV file and input them into newly created vector

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ using std::cout;
 using std::endl;
 
 
-// classes
+/** classes **/
 
 // CrimeCode Class
 class CrimeCode
@@ -50,24 +50,76 @@ class Crime
     public:
         string DateTime;
         string IUCR;
-        bool  Arrest;
-        bool DomesticViolence;
+        string  Arrest;    // TRUE OR FALSE
+        string DomesticViolence;   // TRUE OR FALSE
         int Beat;
         int District;
         int Ward;
         int Community;
         int Year;
     
-    Crime(string datetime, string iucr, bool arrest, bool domestic, int beat, int district, int ward, int community, int year)
+    Crime(string datetime, string iucr, string arrest, string domestic, int beat, int district, int ward, int community, int year)
         : DateTime(datetime), IUCR(iucr), Arrest(arrest), DomesticViolence(domestic), Beat(beat), District(district), Ward(ward),
             Community(community), Year(year)
     { }
 };
 
+/** End of Classes **/
+
+/*
+ * date_range function
+ *
+ * Function outputs the range of the dates which marks from one period to another
+ */
+void date_range()
+{
+
+}
+// End of date_range function
+
 // main Function
 int main()
 {
-    // Open crime code files csv file 
-    ifstream crime_code_file("crime-codes.csv");
+    // Print title of Program
+    cout << "** Crime Analysis **" << endl;
+
+    // Open crime.csv file 
+    ifstream crime_file("crimes.csv");
+    string line, date_time, iucr, arrest, domestic, beat, district, ward, community, year;
+
+    if(!crime_file.good()){
+        cout << "cannot open file!" << endl;
+        return -1;
+    }
+
+    // Input data into a vector of crime objects:
+    vector<Crime> crimes;
+    
+    getline(crime_file, line);  // skip header
+
+    while( getline(crime_file, line)){
+        
+        stringstream ss(line);  // Use stringstream to help parse CSV file
+        
+        // parse line:
+        getline(ss, date_time, ',');
+        getline(ss, iucr, ',');
+        getline(ss, arrest, ',');
+        getline(ss, domestic, ',');
+        getline(ss, beat, ',');
+        getline(ss, district, ',');
+        getline(ss, ward, ',');
+        getline(ss, community, ',');
+        getline(ss, year);
+
+        Crime C(date_time, iucr,
+                arrest, domestic,
+                stoi(beat), stoi(district),
+                stoi(ward), stoi(community),
+                stoi(year));
+
+        // insert @ end;
+        crimes.push_back(C); 
+    }
 }
 // End of main Function
